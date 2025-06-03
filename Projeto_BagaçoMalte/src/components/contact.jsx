3fafaaa66df46707a7d58e6f430935fcf7c2f954
Gabriel Sena <85597607+NeoPato@users.noreply.github.com>
2025-06-03 17:30:28 -0300

    Update contact.jsx

--- Projeto_BagaçoMalte/src/components/contact.jsx
+++ Projeto_BagaçoMalte/src/components/contact.jsx
@@ -7,7 +7,7 @@ export default function Contact() {
             <div id="contact" className="contact">
             <p>Para mais informações, entre em contato conosco:</p>
             <div className="contact-image">
-                <img src="\public\luiz_ft.webp" alt="ft_membro1" />
+                <img src="public\luiz_ft.webp" alt="ft_membro1" />
                 <img src="\public\math_ft.webp" alt="ft_membro2" />
                 <img src="\public\geo_ft.webp" alt="ft_membro3" />
                 <img src="\public\gab_ft.webp" alt="ft_membro4" />

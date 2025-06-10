26c9b5e1f705f031e157ba61a351bb9657d7b247
NeoPato <gabrielhmsena20@gmail.com>
2025-06-10 12:30:13 -0300

    objetivo

--- Projeto_BagaçoMalte/src/components/objective.jsx
+++ Projeto_BagaçoMalte/src/components/objective.jsx
@@ -7,8 +7,12 @@ export default function Objective() {
           <div class="objective-list">
             <div class="objective-item">
               <h4>Farinha e Barra de Cereais</h4>
-              <p>Baseado nas pesquisas feitas, a produção de panificados é a alternativa com uma vasta opções de produtos comérciais
-              ou vendas da farinha em embalagens para o consumidor</p>
+              <p>Nossa proposta para o reaproveitamento do bagaço de malte é a produção de Farinha a 
+                partir do bagaço de malte, para produtos panificados ou venda direta, visto seu valor 
+                nutricional, há enorme potencial no repertório alimentar diário para pessoas saudáveis 
+                ou com condições que causam transtornos alimentares, como anorexia e diabetes. 
+                Além disso, temos como alternativa, a inclusão do bagaço de malte em mix de cereais, 
+                como a barrinha de cereal, testada na prática pela nossa equipe!</p>
             </div>
           </div>
         </section>

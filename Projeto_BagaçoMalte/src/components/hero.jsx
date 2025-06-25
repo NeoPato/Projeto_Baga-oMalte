fca9d820ed8de10cb977ceafe8707565103e4e0c
Gabriel Sena <85597607+NeoPato@users.noreply.github.com>
2025-06-25 19:30:32 -0300

    Update hero.jsx

--- Projeto_BagaçoMalte/src/components/hero.jsx
+++ Projeto_BagaçoMalte/src/components/hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
         <section className="hero">
           <p>O bagaço de Malte é um subproduto da produção cervejeira, gerado na etapa da mosturação e disponível durante todo o ano. 
             Além disso, esse resíduo tem grande potencial na indústria alimentícia, principalmente em relação à alta disponibilidade, 
-            grande quantidade de fibras (em torno de 40%) e valor nutricional.</p>
+            grande quantidade de fibras (em torno de 60%) e valor nutricional.</p>
           <img src="\UniSenai_logo.webp" alt="logo-uni"/>
         </section>
       </section>
